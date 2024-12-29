bebdbd55e2fea39c0251dbabdb12dd84df5f7983
Danbix <d4nbi@icloud.com>
2024-12-29 14:16:10 +0100

    position controller

--- Ambulance_Spring/src/main/java/com/example/ambulance_spring/controllers/PositionController.java
+++ Ambulance_Spring/src/main/java/com/example/ambulance_spring/controllers/PositionController.java
@@ -4,43 +4,46 @@ import com.example.ambulance_spring.dto.MissionRequest;
 import com.example.ambulance_spring.dto.MissionUpdate;
 import com.example.ambulance_spring.entities.Mission;
 import com.example.ambulance_spring.entities.Position;
-import com.example.ambulance_spring.entities.enums.MissionStatus;
 import com.example.ambulance_spring.services.MissionService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+@Slf4j
 @Controller
 public class PositionController {
 
-    @Autowired
-    private MissionService missionService;
+    private final MissionService missionService;
+
+    // Constructor injection
+    public PositionController(MissionService missionService) {
+        this.missionService = missionService;
+    }
 
     @MessageMapping("/position")
     @SendTo("/map/positions")
     public Position send(Position position) {
-        System.out.println("Position received: " + position.getId());
+        log.info("Position received: {}", position.getId());
         return new Position(position.getId(), position.getLatitude(), position.getLongitude());
     }
 
     @MessageMapping("/driver/{id}")
     @SendTo("/map/driver/{id}")
     public Mission assignMission(@Payload MissionRequest mission, @DestinationVariable String id) {
-        System.out.println("Assigning mission to driver ID: " + id);
-        Mission createdMission = missionService.createMission(mission);
-        return createdMission;
+        log.info("Assigning mission to driver ID: {}", id);
+        // Immediately return instead of using a temporary variable
+        return missionService.createMission(mission);
     }
 
     @MessageMapping("/mission/{id}")
     @SendTo("/map/mission/{id}")
     public Mission updateMission(@Payload MissionUpdate update, @DestinationVariable String id) {
-        System.out.println("Updating mission to driver ID: " + id);
-        Mission updatedMission = missionService.updateStatus(Long.parseLong(id), update.getStatus());
-        return updatedMission;
+        log.info("Updating mission to driver ID: {}", id);
+        // Immediately return instead of using a temporary variable
+        return missionService.updateStatus(Long.parseLong(id), update.getStatus());
     }
-
 
 }
